9f7b01af45be3c28fcf3699bd056215b0ea3f2b7
Rose Ames <rose@happyspork.com>
2014-03-03 16:04:03 -0500

    make test files somewhat less agonizing to use

--- testdht.js
+++ testdht.js
@@ -2,4 +2,18 @@ dht = require('./dht');
 
 dht.startup();
 
+console.log("********************");
+console.log("TESTING:  get FFFF00");
+console.log("********************");
+dht.get('ffff00', console.log);
+
+console.log("********************");
+console.log("TESTING:  get FF00FF");
+console.log("********************");
 dht.get('ff00ff', console.log);
+
+// console.log("********************");
+// console.log("TESTING:  get FFFFFF");
+// console.log("********************");
+// dht.get('ff00ff', console.log);
+
