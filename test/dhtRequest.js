262fc361a3dee361dd3962f1345d96c52890d280
Andree Monette <andreecmonette@gmail.com>
2014-03-05 12:21:57 -0500

    edit request test

--- test/dhtRequest.js
+++ test/dhtRequest.js
@@ -1,6 +1,6 @@
 dht = require('../dht');
 
 console.log("********************");
-console.log("TESTING:  get ff0002");
+console.log("TESTING:  get 2000ff");
 console.log("********************");
-dht.get('ff0002', console.log);
+dht.get('2000ff', console.log);
