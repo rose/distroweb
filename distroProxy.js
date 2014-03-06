dfceab17bad1ae7418f7d892dd6c13768607ff63
Andree Monette <andreecmonette@gmail.com>
2014-03-06 15:10:50 -0500

    proxy + dht = happiness

--- distroProxy.js
+++ distroProxy.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var util = require('./utils');
-
+var dht = require('./dht');
 var browserPort = 1234; // will be 80 someday!
 
 
@@ -9,7 +9,12 @@ var browserHandler = function (req, res) {
   if (req.url.match(/^\/distroweb(\/|$)/)) {
     distroReq = req.url.match(/^\/distroweb(.*)/)[1];
     console.log("Proxy:  DistroWeb request: " + distroReq);
-    util.makeTrackRequest(distroReq.substr(1));
+    //util.makeTrackRequest(distroReq.substr(1));
+    dht.get(distroReq.substr(1), function(returnedPage) {
+      res.writeHead(200, { "Content-Type: text/html"});
+      res.write(returnedPage);
+      res.end();
+    });
   } else {
     console.log("Proxy: WWW request: " + req.url);
     redirectToWeb(req.url, res);
