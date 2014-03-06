ca7c4e4ff86d57ac274915d6c5cfe912f6894cba
Rose Ames <rose@happyspork.com>
2014-03-06 13:39:03 -0500

    export processRequest LIKE A BOSS

--- tumour.js
+++ tumour.js
@@ -1,11 +1,11 @@
 var net = require('net');
 
 var tryConnecting = function(peer, request, peerIndex) {
-  console.log("DHT:  Passing on request to " 
+  console.log("Tumour:  Passing on request to " 
       + peer.key + " at " + peer.ip + ":" + peer.port);
 
   var conn = net.createConnection(peer.port, peer.ip, function() {
-    console.log("DHT:  Created connection to " 
+    console.log("Tumour:  Created connection to " 
       + peer.key + " at " + peer.ip + ":" + peer.port);
     conn.write(JSON.stringify(request) + "distro");
   });
@@ -19,14 +19,14 @@ var tryConnecting = function(peer, request, peerIndex) {
   conn.on('data', function(chunk) {
     response += chunk;
     if (response.substr(0,hashLen) == "distro") {
-      console.log("DHT:  Valid response from peer, will not try next");
+      console.log("Tumour:  Valid response from peer, will not try next");
       tryNextPeer = false;
     }
   });
   
 
   conn.on('timeout', function() {
-    console.log("DHT:  TIMEOUT!!!");
+    console.log("Tumour:  TIMEOUT!!!");
 
     if (tryNextPeer) {
       checkNextPeer(peers, request, peerIndex + 1);
@@ -37,7 +37,7 @@ var tryConnecting = function(peer, request, peerIndex) {
   });
 
   conn.on('error', function(err) {
-    console.log("DHT:  ERROR!!! " + err.toString());
+    console.log("Tumour:  ERROR!!! " + err.toString());
 
     if (tryNextPeer) {
       checkNextPeer(peers, request, peerIndex + 1);
@@ -55,11 +55,11 @@ var processRequest = function(peers, request) {
 
 var checkNextPeer = function(peers, request, nextPeer) {
   next = peers[nextPeer];
-  console.log("DHT:  Checking peer " + nextPeer + ": " + next.id + " at " + next.ip);
+  console.log("Tumour:  Checking peer " + nextPeer + ": " + next.id + " at " + next.ip);
 
   if (next.ip === 'localhost') {
-    console.log("DHT:  I am the closest to hash " + request.hash + "!");
-    console.log("DHT:  BOW DOWN BEFORE ME PEASANTS");
+    console.log("Tumour:  I am the closest to hash " + request.hash + "!");
+    console.log("Tumour:  BOW DOWN BEFORE ME PEASANTS");
     request.ports.pop();
     request.outbound = false;
     request.data = tracker.execute(request.hash, request.message);
@@ -70,3 +70,4 @@ var checkNextPeer = function(peers, request, nextPeer) {
   tryConnecting(next, request, nextPeer);
 }
 
+exports.processRequest = processRequest;
